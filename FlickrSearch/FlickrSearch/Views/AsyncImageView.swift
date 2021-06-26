ea66b01772faf97c990948e3636997b49476e4d0
Isuru Nanayakkara <isuru@cenango.lk>
2021-06-26 22:54:50 +0530

    Caching photos.

--- FlickrSearch/FlickrSearch/Views/AsyncImageView.swift
+++ FlickrSearch/FlickrSearch/Views/AsyncImageView.swift
@@ -34,33 +34,39 @@ class AsyncImageView: UIImageView {
 
 // MARK: - Public API
 extension AsyncImageView {
-    func loadImage(from url: URL, placeholder: UIImage? = nil, showActivityIndicator: Bool = true) {
-        image = placeholder
-        
-        activityIndicatorView.isHidden = !showActivityIndicator
-        activityIndicatorView.startAnimating()
-        
+    func loadImage(from url: URL, placeholder: UIImage? = nil, cache: URLCache? = nil, showActivityIndicator: Bool = true) {
+        let cache = cache ?? URLCache.shared
         let request = URLRequest(url: url)
         
-        let config = URLSessionConfiguration.default
-        config.requestCachePolicy = .reloadIgnoringLocalCacheData
-        config.urlCache = nil
-        
-        let session = URLSession(configuration: config)
-        session.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                self.activityIndicatorView.stopAnimating()
-            }
-            if let imageData = data, let image = UIImage(data: imageData) {
+        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+            // If image data exists in cache, load image from there
+            self.image = image
+        } else {
+            self.image = placeholder
+            
+            activityIndicatorView.isHidden = !showActivityIndicator
+            activityIndicatorView.startAnimating()
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
                 DispatchQueue.main.async {
-                    self.image = image
+                    self.activityIndicatorView.stopAnimating()
                 }
-            }
-        }.resume()
+                
+                if let imageData = data, let image = UIImage(data: imageData), let response = response {
+                    // Save image data to cache
+                    let cacheResponse = CachedURLResponse(response: response, data: imageData)
+                    cache.storeCachedResponse(cacheResponse, for: request)
+                    
+                    DispatchQueue.main.async {
+                        self.image = image
+                    }
+                }
+            }.resume()
+        }
     }
     
-    func loadImage(from urlString: String, placeholder: UIImage? = nil, showActivityIndicator: Bool = true) {
+    func loadImage(from urlString: String, placeholder: UIImage? = nil, cache: URLCache? = nil, showActivityIndicator: Bool = true) {
         guard let url = URL(string: urlString) else { return }
-        loadImage(from: url, placeholder: placeholder, showActivityIndicator: showActivityIndicator)
+        loadImage(from: url, placeholder: placeholder, cache: cache, showActivityIndicator: showActivityIndicator)
     }
 }
